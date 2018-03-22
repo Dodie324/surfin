908962d104912a0c9b91376619c81b11422ae7c1
Dodie Moore <dmoore@everquote.com>
2018-03-22 11:45:59 -0400

    write memoized fn to cache comments and author videos data if video detail page request was made within 60 secs

--- src/store/ducks/pageDetails/index.js
+++ src/store/ducks/pageDetails/index.js
@@ -3,13 +3,15 @@ import axios from "axios";
 const LOAD_VIDEO_DETAILS = "LOAD_VIDEO_DETAILS";
 const RETURN_TO_VIDEOS = "RETURN_TO_VIDEOS";
 
-const YOUTUBE_COMMENTS_URI =
-  "https://www.googleapis.com/youtube/v3/commentThreads";
-const YOUTUBE_SEARCH_URI = "https://www.googleapis.com/youtube/v3/search";
 const YOUTUBE_PARAMS = {
   key: process.env.REACT_APP_GOOGLE_API_KEY,
   part: "snippet"
 };
+
+const apiURI = {
+  comments: "https://www.googleapis.com/youtube/v3/commentThreads",
+  videos: "https://www.googleapis.com/youtube/v3/search"
+}
 
 const INITIAL_STATE = {
   authorVideos: [],
@@ -19,27 +21,55 @@ const INITIAL_STATE = {
   videoId: ""
 };
 
+function timeElapsed(startTime) {
+  const endTime = new Date();
+  const timeDiff = (endTime - startTime) / 1000;
+  return Math.round(timeDiff);
+}
+
+const memoizedData = (field) => {
+  let loadedAt = new Date();
+  let cache = {};
+  return async (id) => {
+    const timeDiff = timeElapsed(loadedAt);
+
+    // return cache if pressent and the request is made in less than a minute
+    if (id in cache && timeDiff < 60) {
+      return cache[id];
+    } else {
+      let params = {};
+      loadedAt = new Date();
+
+      if (field === 'comments') {
+        params = {
+          ...YOUTUBE_PARAMS,
+          videoId: id
+        };
+      } else {
+        params = {
+          ...YOUTUBE_PARAMS,
+          channelId: id,
+          maxResults: 10
+        };
+      }
+
+      const { data: { items } } = await axios.get(apiURI[field], { params });
+
+      cache[id] = items;
+      return items;
+    }
+  }
+}
+
+const cachedComments = memoizedData("comments");
+const cachedAuthorVideos = memoizedData("videos");
+
 export const loadVideoDetailPage = (
   videoId,
   videoPageDetails
-) => async dispatch => {
-  const { data: { items: comments } } = await axios.get(YOUTUBE_COMMENTS_URI, {
-    params: {
-      ...YOUTUBE_PARAMS,
-      videoId
-    }
-  });
-
-  const { data: { items: authorVideos } } = await axios.get(
-    YOUTUBE_SEARCH_URI,
-    {
-      params: {
-        ...YOUTUBE_PARAMS,
-        channelId: videoPageDetails.channelId,
-        maxResults: 10
-      }
-    }
-  );
+) => async (dispatch, { pageDetails }) => {
+  const comments = await cachedComments(videoId);
+  const authorVideos = await cachedAuthorVideos(videoPageDetails.channelId);
 
   dispatch({
     type: LOAD_VIDEO_DETAILS,
